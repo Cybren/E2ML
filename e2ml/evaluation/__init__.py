1e6372e6492a9c7a500d264a6e789ecf2c93559f
Cybren <Ben-Gerhards@t-online.de>
2023-06-08 22:59:23 +0200

    add 08

--- e2ml/evaluation/__init__.py
+++ e2ml/evaluation/__init__.py
@@ -1,4 +1,3 @@
-from ._visualization import plot_decision_boundary
-from ._loss_functions import binary_cross_entropy_loss, zero_one_loss
-
-__all__ = ['plot_decision_boundary']
+from ._loss_functions import *
+from ._visualization import *
+from ._performance_measures import *
